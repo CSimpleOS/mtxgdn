44753ab815d3080b601e450dd1162c252bf578c6
dnjhy <ioii24@outlook.com>
2025-07-22 17:52:04 +0800

    V1.0.10

---
+++ src/main/java/net/mcreator/mtxgdn/procedures/Test1DangWanJiaChiKaiWeiDuShiProcedure.java
@@ -0,0 +1,17 @@
+package net.mcreator.mtxgdn.procedures;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+
+public class Test1DangWanJiaChiKaiWeiDuShiProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof Player _player) {
+			ItemStack _stktoremove = new ItemStack(Blocks.BEDROCK);
+			_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/mtxgdn/world/dimension/Test1Dimension.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.mtxgdn.world.dimension;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+
+import net.mcreator.mtxgdn.procedures.Test1DangWanJiaJinRuWeiDuShiProcedure;
+import net.mcreator.mtxgdn.procedures.Test1DangWanJiaChiKaiWeiDuShiProcedure;
+
+@Mod.EventBusSubscriber
+public class Test1Dimension {
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class DimensionSpecialEffectsHandler {
+		@SubscribeEvent
+		@OnlyIn(Dist.CLIENT)
+		public static void registerDimensionSpecialEffects(RegisterDimensionSpecialEffectsEvent event) {
+			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(128, true, DimensionSpecialEffects.SkyType.NORMAL, false, false) {
+				@Override
+				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
+					return color.multiply(sunHeight * 0.94 + 0.06, sunHeight * 0.94 + 0.06, sunHeight * 0.91 + 0.09);
+				}
+
+				@Override
+				public boolean isFoggyAt(int x, int y) {
+					return false;
+				}
+			};
+			event.register(new ResourceLocation("mtxgdn:test_1"), customEffect);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPlayerChangedDimensionEvent(PlayerEvent.PlayerChangedDimensionEvent event) {
+		Entity entity = event.getEntity();
+		Level world = entity.level();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		if (event.getFrom() == ResourceKey.create(Registries.DIMENSION, new ResourceLocation("mtxgdn:test_1"))) {
+
+			Test1DangWanJiaChiKaiWeiDuShiProcedure.execute(entity);
+		}
+		if (event.getTo() == ResourceKey.create(Registries.DIMENSION, new ResourceLocation("mtxgdn:test_1"))) {
+
+			Test1DangWanJiaJinRuWeiDuShiProcedure.execute(entity);
+		}
+	}
+}
